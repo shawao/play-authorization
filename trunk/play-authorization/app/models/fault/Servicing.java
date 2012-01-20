0910c2c6e8dfdc5e58444f8122dacc6c0619ad29
ember319 <ember319@92a7ded2-c2c8-ef87-b2b0-1bd448d5f176>
2012-01-20 07:47:10 +0000

    修正数据库模型

--- trunk/play-authorization/app/models/fault/Servicing.java
+++ trunk/play-authorization/app/models/fault/Servicing.java
@@ -1,9 +1,11 @@
 package models.fault;
 
 import models.AbstractEntity;
+import models.sys.SysUser;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.util.Date;
 
@@ -19,33 +21,46 @@ import java.util.Date;
 @Table(name = "t_servicing")
 public class Servicing extends AbstractEntity {
 
-    @Column(name="sta_id")
-    public Long stationId;
-    @Column(name = "fau_id")
-    public Long faultId;
+    @ManyToOne(optional = false)
+    public AutoStation station;
+
+    @ManyToOne(optional = false)
+    public Fault fault;
 
     // todo:维修可以多个人？！
-    @Column(nullable = false)
-    public Long userId;//登录人
-    public Long userId2;
-    public Long userId3;
-    public Long userId4;
+    @ManyToOne(optional = false)
+    public SysUser user;//登录人, major
 
-    @Column(name = "serv_start_time")
-    public Date serviceStartTime;
-    @Column(name = "serv_end_time")
-    public Date serviceEndTime;
+    @ManyToOne
+    public SysUser user2;
+
+    @ManyToOne
+    public SysUser user3;
+
+    @Column
+    public Date startTime;
+    
+    @Column
+    public Date endTime;
 
     @Column(name = "proc",length = 1000)
     public String process;//维修过程
+    
     @Column(name = "result",length = 1000)
     public String result;//文字描述维修结果
-    
-    public Long reasonId;//关联常量表中的故障根源类型的某ID
+
+    @ManyToOne
+    public SysConstant reason;//关联常量表中的故障根源类型的某ID
+
+
     @Column(length = 1000)
     public String remark;//其他描述
 
     public int fixed;//1:yes, 2:no，是否修好
+    
     public int audited;//0:待审核 1:通过, 2:未通过, 是否通过审核
+
+    @Column(length = 500)
+    public String auditComment;
 
 }
